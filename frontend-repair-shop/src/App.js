98508519164a07bdb544590a56653d8ed417eec2
Nebojsa Brankovic <n.brankovic99@gmail.com>
2023-08-24 20:08:38 +0200

    Added bootstrap and made Navbar and Home page

--- frontend-repair-shop/src/App.js
+++ frontend-repair-shop/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Page">
+      <BrowserRouter className="App">
+        <div className="NavBar">
+          <Navbar />
+        </div>
+        <div className="Main">
+          <div className="CenterComponent">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend-repair-shop/src/components/Navbar.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
+function TopNavbar() {
+  if (localStorage.getItem("token") != null) {
+    return (
+      <Navbar
+        fixed="top"
+        bg="dark"
+        data-bs-theme="dark"
+        expand="lg"
+        className="bg-body-tertiary"
+      >
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              alt=""
+              src="/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Mechanic's Store
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <NavDropdown title="Brands" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/brands">See all</NavDropdown.Item>
+                <NavDropdown.Item href="/brands/add">Add</NavDropdown.Item>
+                <NavDropdown.Item href="/brands/delete">
+                  Delete
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Models" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/models">See all</NavDropdown.Item>
+                <NavDropdown.Item href="/models/add">Add</NavDropdown.Item>
+                <NavDropdown.Item href="/models/delete">
+                  Delete
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Engines" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/engines">See all</NavDropdown.Item>
+                <NavDropdown.Item href="/engines/add">Add</NavDropdown.Item>
+                <NavDropdown.Item href="/engines/delete">
+                  Delete
+                </NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Cars" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/cars">See all</NavDropdown.Item>
+                <NavDropdown.Item href="/cars/add">Add</NavDropdown.Item>
+                <NavDropdown.Item href="/cars/delete">Delete</NavDropdown.Item>
+              </NavDropdown>
+              <NavDropdown title="Reservations" id="basic-nav-dropdown">
+                <NavDropdown.Item href="/reservations">
+                  See all
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reservations/add">
+                  Add
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/reservations/delete">
+                  Delete
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="/login">Log out</Nav.Link>
+            </Nav>
+            <Navbar.Toggle />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default TopNavbar;
